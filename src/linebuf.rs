f34fc3575b7e2ca7b9b2529f28f60334eab07106
Matt Mastracci <matthew@mastracci.com>
2020-05-18 11:26:31 -0600

    LineBuf completed

--- src/linebuf.rs
+++ src/linebuf.rs
@@ -1,6 +1,7 @@
 pub struct LineBuf<T: FnMut(String)> {
     accept_lines: T,
     buf: Vec<u8>,
+    buffer_size: usize,
 }
 
 const LF: u8 = '\n' as u8;
@@ -8,23 +9,25 @@ const LF: u8 = '\n' as u8;
 impl<T: FnMut(String)> LineBuf<T> {
     pub fn new(buffer_size: usize, accept_lines: T) -> Self {
         let buf = Vec::with_capacity(buffer_size);
-        LineBuf { accept_lines, buf }
+        LineBuf { accept_lines, buf, buffer_size }
     }
 
     pub fn accept(&mut self, bytes: &[u8]) {
+        debug_assert!(self.buf.capacity() == self.buffer_size);
         for b in bytes.iter() {
             if *b == LF {
                 let mut out = self.buf.split_off(0);
                 out.push(LF);
                 (self.accept_lines)(String::from_utf8_lossy(&out).into());
             } else {
-                self.buf.push(*b);
                 if self.buf.len() == self.buf.capacity() {
                     (self.accept_lines)(String::from_utf8_lossy(&self.buf).into());
                     self.buf.clear();
                 }
+                self.buf.push(*b);
             }
         }
+        debug_assert!(self.buf.capacity() == self.buffer_size);
     }
 
     pub fn close(mut self) {
@@ -57,10 +60,39 @@ pub mod tests {
     #[test]
     fn test_long_lines() {
         test(
-            "0123456789012345678901234567890123456789012345678901234567890123456789",
+            "01234567890123456789012345678901234567890123456789012345678901234567890123456789",
             vec![
                 "0123456789012345678901234567890123456789",
                 "0123456789012345678901234567890123456789",
+            ],
+        );
+        test(
+            "012345678901234567890123456789012345678901234567890123456789012345678901234567890",
+            vec![
+                "0123456789012345678901234567890123456789",
+                "0123456789012345678901234567890123456789",
+                "0",
+            ],
+        );
+        test(
+            "0123456789012345678901234567890123456789\n0123456789012345678901234567890123456789",
+            vec![
+                "0123456789012345678901234567890123456789\n",
+                "0123456789012345678901234567890123456789",
+            ],
+        );
+        test(
+            "0123456789012345678901234567890123456789\n0123456789012345678901234567890123456789\n",
+            vec![
+                "0123456789012345678901234567890123456789\n",
+                "0123456789012345678901234567890123456789\n",
+            ],
+        );
+        test(
+            "01234567890123456789012345678901234567890123456789012345678901234567890123456789\n",
+            vec![
+                "0123456789012345678901234567890123456789",
+                "0123456789012345678901234567890123456789\n",
             ],
         );
     }

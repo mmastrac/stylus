6a955c8ab838eeab72b92d40eba59723dbd9b2c7
Matt Mastracci <matthew@mastracci.com>
2020-05-18 15:58:38 -0600

    Adding set -xeuf -o pipefail

--- example/monitor.d/router/test.sh
+++ example/monitor.d/router/test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+set -xeuf -o pipefail
 ping -c 1 8.8.8.8
--- example/monitor.d/server/test.sh
+++ example/monitor.d/server/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -xeuf -o pipefail
 echo Curling
 curl --retry 2 --max-time 5 --connect-timeout 5 http://192.168.1.1:9000
